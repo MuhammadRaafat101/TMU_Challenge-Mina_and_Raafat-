b11cc45a15c592ba33a40eaa1c6fb53e8bb89603
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-15 16:56:56 +0200

    Create TMU_PREcfg.h

---
+++ TMU_PREcfg.h
@@ -0,0 +1,14 @@
+/*
+ * TMU_PREcfg.h
+ *
+ *  Created on: Oct 15, 2019
+ *      Author: Rafaat
+ */
+
+#ifndef TMU_PRECFG_H_
+#define TMU_PRECFG_H_
+
+/****** NumberOF Consumers can be defined *******/
+#define 	NumOfTasks	2
+
+#endif /* TMU_PRECFG_H_ */
