93345d3bce8183fc9b25f3de18f10840da4de0b4
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-15 16:56:53 +0200

    Create TMU_PBcfg.h

---
+++ TMU_PBcfg.h
@@ -0,0 +1,17 @@
+/*
+ * TMU_PBcfg.h
+ *
+ *  Created on: Oct 15, 2019
+ *      Author: Rafaat
+ */
+
+#ifndef TMU_PBCFG_H_
+#define TMU_PBCFG_H_
+
+#include "Std_Types.h"
+#include "TMU.h"
+
+typedef	enum{TIMER0,TIMER1,TIMER2,NUM_OF_TIMER}Timer; //this enum for Timer Selection
+
+
+#endif /* TMU_PBCFG_H_ */
