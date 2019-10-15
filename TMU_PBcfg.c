f02d1cd58123fffbcd58014f8127b2308e73aecf
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-10-15 16:56:50 +0200

    Create TMU_PBcfg.c

---
+++ TMU_PBcfg.c
@@ -0,0 +1,13 @@
+
+#include"TMU_PBcfg.h"
+
+
+
+/*************TMU_Config**********/
+/*
+ * parameter 1 is for selecting Timer
+ * parameter 2 is for Reselusion it can be an integer number from 1 -----> 255
+ *
+ */
+
+TMU_Config_t TMU_Config={TIMER0,1};
